1737536c2dc1730b5d0f280d4410560e6c98e32d
John Duda <john@sylvie.(none)>
2011-12-27 21:59:25 -0500

    It's Rails!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freeskool::Application.config.secret_token = '351b469731ae7da7068052830c1cbb58c1d8371ec9e14fadf05f4c6df57ac7fd2a34de94009bf9bfc5d226d0e69a971530f301ae00fabfcbcd6b27d8eac616d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Freeskool::Application.config.session_store :cookie_store, :key => '_freeskool_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Freeskool::Application.config.session_store :active_record_store
