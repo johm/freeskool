44aab014f08be0e40ec283a6fbaf1048e49e92af
John Duda <john@manifestor.org>
2012-02-16 18:04:26 -0500

    John did some work on the front page, and a plugin designed to make it easy to sanitize html input from users

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,6 @@
 class Course < ActiveRecord::Base
+  attr_protected :is_approved
+  clean_up_html [:short_description,:long_description]
+
+
 end
---
+++ vendor/plugins/cleanhtml/test/cleanhtml_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CleanhtmlTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
