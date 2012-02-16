8f7deea390ea2b4cd4f98cea937931f867af6ea7
Jason Lewis <jlewis73@jhu.edu>
2012-02-16 14:55:03 -0500

    Squeel init, add Location#is_available?

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,5 +4,9 @@ class Location < ActiveRecord::Base
   has_many :courses, :through => :sessions
   
   
+  def is_available?(start_t, end_t)
+    self.sessions.where{(start_time >= start_t) & (start_time <= end_t) | (end_time <= end_t) & (end_time >= start_t)}.any?
+  end
+  
   
 end
