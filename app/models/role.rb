37895b4b959ce196f7113c971a14b9b0297d0150
Jason Lewis <jlewis73@jhu.edu>
2012-02-16 13:18:11 -0500

    Add Role for CanCan auth

---
+++ app/models/role.rb
@@ -0,0 +1,11 @@
+##
+# Roles for CanCan authorization. 
+# Roles should probably include Admin, Instructor, Student, more as needed.
+
+class Role < ActiveRecord::Base
+  
+  
+  
+  has_and_belongs_to_many :users
+  
+end
---
+++ db/migrate/20120216180730_create_roles.rb
@@ -0,0 +1,22 @@
+class CreateRoles < ActiveRecord::Migration
+  def self.up
+    create_table :roles do |t|
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+    
+    create_table :roles_users, :id => false do |t|
+      t.integer :role_id, :null => false
+      t.integer :user_id, :null => false
+    end
+    
+    add_index(:roles_users, [:role_id, :user_id], :unique => true)
+  end
+
+  def self.down
+    drop_table :roles
+    drop_table :roles_users
+  end
+end
