17d53e6b3f9757205fc81dff1461a91c9b99e178
Jason Lewis <jlewis73@jhu.edu>
2012-02-16 13:27:42 -0500

    Add Admin role to ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,9 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    if user.has_role? :admin
+      can :manage, :all
+    end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
