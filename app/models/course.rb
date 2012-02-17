2e50534574ace345872d08437e4da5a419414aa8
Jason Lewis <jlewis73@jhu.edu>
2012-02-17 11:30:15 -0500

    Add associations on Course to have students

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,12 @@ class Course < ActiveRecord::Base
   
   has_many :sessions
   has_many :locations, :through => :sessions
+  
+  has_many :signups
+  
+  belongs_to :user
+  
+  has_many :students, :through => :signups, :class_name => 'User'
 
   attr_protected :is_approved
   clean_up_html [:short_description,:long_description]
