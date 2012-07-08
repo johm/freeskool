a9f88058e81852f52e6bf27784afe81c33f0a03d
John Duda <john@manifestor.org>
2012-07-08 14:23:33 -0400

    frontend changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
   def is_mine(something)
-    user_signed_in? && something.instructor.id==current_user.id
+    user_signed_in? && (! something.instructor.nil?) && something.instructor.id==current_user.id
   end
   
   def taking_this(course)
