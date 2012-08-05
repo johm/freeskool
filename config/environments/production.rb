17226f6d43eba0bf4a5c95d8a0064e40cbc20480
John Duda <john@manifestor.org>
2012-08-04 20:21:21 -0400

    small fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,9 @@ Freeskool::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+    config.assets.precompile += %w(rails_admin/rails_admin.css rails_admin/rails_admin.js ckeditor/*)
+
+
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
