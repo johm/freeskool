dec83273d4bf6913ec2f98e9fe4f49ccffb0a194
John Duda <john@manifestor.org>
2012-08-11 22:43:25 -0400

    order before array convert

--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -1,7 +1,7 @@
 class FrontpageController < ApplicationController
 
   def index
-    @courses=Course.upcoming.approved.ispublic.uniq.order('created_at DESC,id DESC')
+    @courses=Course.upcoming.approved.ispublic.order('created_at DESC,id DESC').uniq
   end
 
 
