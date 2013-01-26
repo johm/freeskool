7dad61fec8e968f67765a5946d5c539e77fbbc85
John Duda <john@sylvie.(none)>
2013-01-26 12:10:38 -0500

    make session creation more prominent

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -70,7 +70,7 @@ class CoursesController < ApplicationController
 
     respond_to do |format|
       if @course.save
-        format.html { redirect_to @course, :notice => 'Course was successfully submitted.  Now indicate which sessions you would like to reserve at the Free School.' }
+        format.html { redirect_to @course, :notice => 'Course was successfully submitted.  <div class="nowdothis">Now indicate which sessions you would like to reserve at the Free School by adding them on the right</div>'.html_safe }
         format.json { render :json => @course, :status => :created, :location => @course }
       else
         format.html { render :action => "new" }
