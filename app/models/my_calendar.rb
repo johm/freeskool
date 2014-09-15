f99b5d8bc15df4ac88937929bad0af49f4ee9d1e
John Duda <john@redemmas.org>
2014-09-15 17:20:59 -0400

    technical fixes are the best fixes

--- app/models/my_calendar.rb
+++ app/models/my_calendar.rb
@@ -7,7 +7,7 @@ class MyCalendar
           event do
             dtstart       s.course_session_start + 5.hours - (s.course_session_start.dst? ? 0.hours : 1.hours)
             dtend         s.course_session_end + 5.hours - (s.course_session_start.dst? ? 0.hours : 1.hours)
-            summary     "Freeschool: #{e.name} {'(proposed)' unless e.approved?}" + 
+            summary     "Freeschool: #{e.name} #{'(proposed)' unless e.approved?}"  
             description "From Freeschool"
           end
         end
